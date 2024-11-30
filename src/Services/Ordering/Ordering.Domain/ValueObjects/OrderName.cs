32c81c61353cec843fc8a19098c32d2021adaaf8
martrzeciak <mariusz.trzeciak09@gmail.com>
2024-11-30 23:46:38 +0100

    Create rich-domain model entity for order entities in Order.Domain

--- src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -2,5 +2,15 @@
 
 public record OrderName
 {
+    private const int DefaultLength = 5;
     public string Value { get; }
+    private OrderName(string name) => Value = name;
+
+    public static OrderName Of(string value)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(value);
+        ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, DefaultLength);
+
+        return new OrderName(value);
+    }
 }
